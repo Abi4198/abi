983b5b6d6823c398c4c0dbd08ac7e4daf944a87c
Abi4198 <144605273+Abi4198@users.noreply.github.com>
2024-02-20 21:58:44 -0800

    Add files via upload

---
+++ student.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registrationform</title>
+    <style>
+        .a 
+        { 
+            float:left;
+            margin-top:4px
+        }
+        .a 
+        {
+            float:left;
+            margin-top:50px;
+            margin-left:200px;
+        }
+        .b 
+        {
+            margin-right:800px;
+            float:right;
+        }
+         </style>
+</head>
+<body>
+   <h1>Registration form</h1>
+     <form action="2nd_1.php"method

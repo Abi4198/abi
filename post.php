bb87e868ca1f8f2b2a2bc9065761e966b19f1ecc
Abi4198 <144605273+Abi4198@users.noreply.github.com>
2024-02-20 21:55:16 -0800

    Add files via upload

---
+++ post.php
@@ -0,0 +1,22 @@
+<?PHP?>
+<head>
+<title>post</title>
+</head>
+<body>
+    <center>
+       <form action="1 page.php"method="post">
+           <div class="box1">
+           <label>1.English</label><input type="text"name="english"placeholder="Enter Your Mark"value="">
+           </div>
+           <div class="box1">
+           <label>2.Tamil</label><input type="text"name="tamil"placeholder="Enter Your Mark"value="">
+           </div>
+           <div class="box1">
+           <label>3.Maths</label><input type="text"name="maths"placeholder="Enter Your Mark"value="">
+           </div>
+           <div class="box1">
+           <label>4.Science</label><input type="text"name="science"placeholder="Enter Your Mark"value="">
+           </div>
+           <div class="box1">
+           <label>5.Social Science</label><input  type=""text"nmae="socila science"placeholder="Enter Your Mark"value="">
+           
